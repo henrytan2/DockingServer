6df4d1bd15c73812dd3af4235ddd8e555c924bee
henrytan2 <henrytan508@gmail.com>
2021-12-14 16:18:07 -0700

    new docking server app

--- DockingServer/asgi.py
+++ DockingServer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DockingServer project.
+ASGI config for dockingserver project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DockingServer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dockingserver.settings')
 
 application = get_asgi_application()
